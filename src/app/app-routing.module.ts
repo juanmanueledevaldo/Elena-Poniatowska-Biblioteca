ef0a06a5708a1050f50084d7bfe5f21134867338
Unknown <juanemijas@gmail.com>
2019-12-06 14:51:26 -0800

    juqn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,11 +26,8 @@ import { AddEditBookComponent } from './Components/add-edit-book/add-edit-book.c
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-<<<<<<< HEAD
+
   { path: 'loans', component: AddEditLoanComponent, canActivate:[AuthAdmin] },
-=======
-  { path: 'loans', component: AddEditLoanComponent},
->>>>>>> develop
   { path: 'cards', component: CardsComponent },
   { path: 'loan', component: LoanComponent, canActivate:[AuthGuard] },
   { path: 'loan/:id', component: LoanComponent, canActivate:[AuthGuard] },
